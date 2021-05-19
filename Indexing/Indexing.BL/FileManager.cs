0fc664001ac9b1f9dd2b8bf4d23093b71abb2aee
Vadym Kolesnyk <vakol2001@gmail.com>
2021-05-19 20:10:37 +0300

    Add FileManager class implementation

---
+++ Indexing/Indexing.BL/FileManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indexing.BL
+{
+    class FileManager
+    {
+        private readonly IEnumerable<string> _files;
+        private readonly IEnumerator<string> _filesEnumerator;
+        private readonly object _locker = new object();
+        public FileManager(string root)
+        {
+            if (string.IsNullOrEmpty(root))
+            {
+                throw new ArgumentException($"\"{nameof(root)}\" can't be null or empty.");
+            }
+            _files = GetAllFiles(root);
+            _filesEnumerator = _files.GetEnumerator();
+        }
+
+        private IEnumerable<string> GetAllFiles(string root)
+        {
+            IEnumerable<string> files = Directory.EnumerateFiles(root);
+            return Directory.EnumerateDirectories(root).Aggregate(files, (files, dir) => files.Concat(GetAllFiles(dir)));
+        }
+        public string GetNextOrNull()
+        {
+            lock (_locker)
+            {
+                if (_filesEnumerator.MoveNext())
+                {
+                    return _filesEnumerator.Current;
+                }
+                return null;
+            }
+
+        }
+    }
+}
