91322e5b0997d52df8747cc205fdc5400ae717d7
Vadym Kolesnyk <vakol2001@gmail.com>
2021-05-20 11:11:53 +0300

    Add Add method tests for Index class

---
+++ Indexing/Indexing.BL.Tests/IndexTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using Xunit;
+
+namespace Indexing.BL.Tests
+{
+    public class IndexTests
+    {
+        [Fact]
+        public void Add_WhenAdd2Files_Then2Files()
+        {
+            Index index = new();
+            index.Add("word1", "file1");
+            index.Add("word1", "file2");
+            Assert.Equal(2, index["word1"].Count());
+        }
+        [Fact]
+        public void Add_WhenAddSameCombinations_Then1File()
+        {
+            Index index = new();
+            index.Add("word1", "file1");
+            index.Add("word1", "file1");
+            Assert.Single(index["word1"]);
+        }
+
+    }
+}
