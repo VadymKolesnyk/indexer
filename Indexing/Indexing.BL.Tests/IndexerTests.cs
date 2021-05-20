59fc9263c5df969e0f276826cf0b70cc27e35bd6
Vadym Kolesnyk <vakol2001@gmail.com>
2021-05-20 14:02:03 +0300

    Add constructor tests for Indexer class

---
+++ Indexing/Indexing.BL.Tests/IndexerTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Xunit;
+
+namespace Indexing.BL.Tests
+{
+    public class IndexerTests : IDisposable
+    {
+        static int _testsCount = 1;
+        private readonly string _directoryPath = $@"{Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)}\TestDirectory_{_testsCount++}";
+        public IndexerTests()
+        {
+            Directory.CreateDirectory(_directoryPath);
+        }
+        public void Dispose()
+        {
+            Directory.Delete(_directoryPath, true);
+        }
+
+        [Fact]
+        public void Indexer_WhenNegativeNumberOfTheads_ThenThrowArgumentException()
+        {
+            Assert.Throws<ArgumentException>(() => new Indexer(_directoryPath, -1));
+        }
+
+
+    }
+}
