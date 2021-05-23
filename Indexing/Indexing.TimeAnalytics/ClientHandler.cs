f13998c5ec6d5d385ecfc54b5279630eb78f363e
Vadym Kolesnyk <vakol2001@gmail.com>
2021-05-23 06:34:12 +0300

    Add ClientHandler class

---
+++ Indexing/Indexing.TimeAnalytics/ClientHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indexing.TimeAnalytics
+{
+    class ClientHendler
+    {
+        readonly Index _index;
+
+        public ClientHendler(Index index)
+        {
+            _index = index;
+        }
+
+        public void Handle()
+        {
+            //TO DO
+        }
+
+        public Task HandleAsync() => Task.Run(Handle);
+
+    }
+}
