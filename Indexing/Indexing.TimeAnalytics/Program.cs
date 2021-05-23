deec19139f7589c5391faad12a69df9b8f994ff7
Vadym Kolesnyk <vakol2001@gmail.com>
2021-05-23 05:51:29 +0300

    Add Program.Main for TimeAnalytics

--- Indexing/Indexing.TimeAnalytics/Program.cs
+++ Indexing/Indexing.TimeAnalytics/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Indexing.BL;
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
 
 namespace Indexing.TimeAnalytics
 {
@@ -6,7 +9,22 @@ namespace Indexing.TimeAnalytics
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(@"Enter directory with files (if empty - D:\datasets\aclImdb)");
+            var input = Console.ReadLine();
+            var path = input == string.Empty ? @"D:\datasets\aclImdb" : input;
+
+            for (int i = 1; i <= 16; i++)
+            {
+                var indexer = new Indexer(path, i);
+                var clock = Stopwatch.StartNew();
+                var task = indexer.GetIndexAsync();
+                task.Wait();
+                clock.Stop();
+                Console.WriteLine($"Path: {path},\t Number of threads : {i,2},\tTime : {clock.Elapsed}");
+                GC.Collect();
+            }
+            Console.ReadLine();
+
         }
     }
 }
