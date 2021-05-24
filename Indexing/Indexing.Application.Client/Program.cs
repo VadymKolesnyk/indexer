219a50bddf991e751cee134d913e29cb6e3df084
Vadym Kolesnyk <vakol2001@gmail.com>
2021-05-24 15:47:39 +0300

    Add basics logic to client project

--- Indexing/Indexing.Application.Client/Program.cs
+++ Indexing/Indexing.Application.Client/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net.Sockets;
 
 namespace Indexing.Application.Client
 {
@@ -6,7 +7,28 @@ namespace Indexing.Application.Client
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            TcpClient client = null;
+            NetworkStream stream = null;
+            Logger logger = new Logger();
+            try
+            {
+                client = new TcpClient("127.0.0.1", 5001);
+                stream = client.GetStream();
+                string[] input = new string[] { "to", "see", "how", "this", "kind" };
+                Messager.Send(stream, new { words = input });
+                var answer = Messager.Recive(stream);
+                Console.WriteLine(answer);
+            }
+            catch(SocketException e)
+            {
+                logger.Log(e.Message);
+            }
+            finally
+            {
+                client?.Close();
+                stream?.Close();
+            }
+            
         }
     }
 }
